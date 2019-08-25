72f7b6c4e846b4ca4b9021637859808ebdee9072
Sinan <me@wunnle.com>
2019-08-25 23:17:34 +0300

    playing with some animations

--- pages/work/[wid].js
+++ pages/work/[wid].js
@@ -1,3 +1,5 @@
+import { useRef, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
 import ReactMarkdown from 'react-markdown'
 import matter from 'gray-matter';
 import styles from './work.module.css'
@@ -5,8 +7,10 @@ import Head from 'next/head';
 
 
 
-const Post = ({ content, data }) => (
-  <>
+const Post = ({ content, data }) => {
+
+
+  return (<>
     <Head>
       <title>{data.title}</title>
       <link href="/static/common.css" rel="stylesheet" />
@@ -19,14 +23,31 @@ const Post = ({ content, data }) => (
         <ReactMarkdown source={content} renderers={{ image: Img }} />
         : 'loading'}
     </article>
-  </>
-)
+  </>)
+}
 
-const Img = ({ alt, src }) => (
-  <div className={styles.imgContainer}>
-    <img src={src} alt={alt} />
-  </div>
-)
+const Img = ({ alt, src }) => {
+
+  const [gotInView, setGotInView] = useState(false)
+
+  const [ref, inView] = useInView({
+    threshold: 0,
+  })
+
+  useEffect(() => {
+    if (inView && !gotInView) {
+      setGotInView(true)
+    }
+  }, [inView])
+
+  console.log({ inView })
+
+  return (
+    <div className={[styles.imgContainer, gotInView ? styles.imgContainerAnim : ''].join(' ')} ref={ref}>
+      <img src={src} alt={alt} />
+    </div>
+  )
+}
 
 Post.getInitialProps = async function ({ query }) {
 
