7729c1754e6cc0130d848909bcebf1896f86b979
Sinan <me@wunnle.com>
2019-08-28 09:27:02 +0300

    changes on listing

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,25 @@ import Head from 'next/head';
 
 
 const Home = ({ pages }) => (
-    <div className={styles.home}>
-      <Head>
-        <title>Home</title>
-        <link href="/static/common.css" rel="stylesheet" />
-      </Head>
-      {console.log}
-      <h1>Pages</h1>
-      {
-        pages.map(p => (
-          <Link href={`/work/${p.slug}`} key={p.slug}>
-            <a className={styles.pageLink}>
-            <h2>{p.slug}</h2>
-            <span>{p.document.data.date}</span>
-            </a>
-          </Link>
-        ))
-      }
-    </div>
-  )
+  <div className={styles.home}>
+    <Head>
+      <title>Home</title>
+      <link href="/static/common.css" rel="stylesheet" />
+    </Head>
+    {console.log}
+    <h1>Selected works</h1>
+    {
+      pages.map(p => (
+        <Link href={`/work/${p.slug}`} key={p.slug}>
+          <a className={styles.pageLink}>
+            <h2>{p.document.data.title}</h2>
+            <span>{p.document.data.year}</span>
+          </a>
+        </Link>
+      ))
+    }
+  </div>
+)
 
 Home.getInitialProps = async function () {
   const pages = await getWorks()
