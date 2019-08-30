e38cbd32a7430a80037392249ca4af0a99f66cac
Sinan <me@wunnle.com>
2019-08-30 21:43:56 +0300

    better css modules naming

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const withImages = require('next-images')
 
 module.exports = withImages(withCSS({
   cssModules: true,
+  cssLoaderOptions: {
+    importLoaders: 1,
+    localIdentName: "[local]___[hash:base64:5]",
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.md$/,
