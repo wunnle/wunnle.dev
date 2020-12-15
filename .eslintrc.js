5e4d1fbaf8475ca0a110b312d810d8de798809dc
Sinan <me@wunnle.com>
2020-12-15 18:20:11 +0300

    upgraded packages and refactored icons

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,39 @@
 module.exports = {
-    "parser": "babel-eslint",
-    "env": {
-        browser: true,
-        node: true,
-        es6: true
-      },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-      ],
-    "plugins": [
-        "simple-import-sort",
-        "prettier",
-        "react-hooks"
-      ],
-      "rules": {
-        "prettier/prettier": [
-          "error",
-          {
-            "trailingComma": "none",
-            "singleQuote": true,
-            "printWidth": 90,
-            "endOfLine":"auto",
-            "semi": false
-          }
-        ],
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "react/react-in-jsx-scope": 0,
-        "quotes": [
-          2,
-          "single",
-          {
-            "avoidEscape": true,
-            "allowTemplateLiterals": true
-          }
-        ],
-        "comma-dangle": 0,
-        "simple-import-sort/sort": "error",
-        "max-len": 0,
-        "multiline-ternary": ["error", "always-multiline"],
-        "no-console": 0,
-        "semi": ["error", "never"]
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  plugins: ['simple-import-sort', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        singleQuote: true,
+        printWidth: 90,
+        endOfLine: 'auto',
+        semi: false
       }
-};
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/react-in-jsx-scope': 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true
+      }
+    ],
+    'comma-dangle': 0,
+    'simple-import-sort/imports': 'error',
+    'max-len': 0,
+    'multiline-ternary': ['error', 'always-multiline'],
+    'no-console': 0,
+    semi: ['error', 'never']
+  }
+}
--- components/Icon/index.js
+++ components/Icon/index.js
@@ -1,14 +1,22 @@
-const Icon = ({ children: icon, className, white }) => (
-  <>
-    {icon ? (
-      <img
-        className={className}
-        title={icon}
-        src={require(`./${icon.toLowerCase() + (white ? 'White' : '')}.svg`)}
-        alt={icon}
-      />
-    ) : null}
-  </>
-)
+import PropTypes from 'prop-types'
+
+import icons from './icons'
+
+const Icon = ({ children: icon, className, white }) => {
+  const name = icon.toLowerCase() + (white ? 'White' : '')
+  const src = icons?.[name]
+
+  if (src) {
+    return <img className={className} title={icon} src={src} alt={icon} />
+  }
+
+  return null
+}
+
+Icon.propTypes = {
+  children: PropTypes.string,
+  className: PropTypes.string,
+  white: PropTypes.bool
+}
 
 export default Icon
